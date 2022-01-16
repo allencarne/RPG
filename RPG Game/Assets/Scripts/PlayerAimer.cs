e2747b142d3d12c658b679459bf39c4aeeb574f6
allencarne <allencarne@yahoo.com>
2022-01-16 11:31:46 -0800

    Code Cleanup
    Changed names of variables and cleaned up code

--- RPG Game/Assets/Scripts/PlayerAimer.cs
+++ RPG Game/Assets/Scripts/PlayerAimer.cs
@@ -7,12 +7,13 @@ public class PlayerAimer : MonoBehaviour
     public float offset;
 
     public Transform firePoint;
-    public GameObject punchPrefab;
+    public GameObject slashPrefab;
+    public Animator animator;
 
-    public float punchForce;
+    public float slashForce;
 
-    private float shootCoolDown;
-    public float startShootCoolDown;
+    private float attackCoolDown;
+    public float startAttackCoolDown;
 
     void Update()
     {
@@ -21,24 +22,25 @@ public class PlayerAimer : MonoBehaviour
         float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
 
-        if (shootCoolDown <= 0)
+        if (attackCoolDown <= 0)
         {
             if (Input.GetMouseButtonDown(0))
             {
-                Shoot();
-                shootCoolDown = startShootCoolDown;
+                Attack();
+                attackCoolDown = startAttackCoolDown;
             }
         }
         else
         {
-            shootCoolDown -= Time.deltaTime;
+            attackCoolDown -= Time.deltaTime;
         }
     }
 
-    void Shoot()
+    void Attack()
     {
-        GameObject punch = Instantiate(punchPrefab, firePoint.position, firePoint.rotation);
+        GameObject punch = Instantiate(slashPrefab, firePoint.position, firePoint.rotation);
         Rigidbody2D rb = punch.GetComponent<Rigidbody2D>();
-        rb.AddForce(firePoint.up * punchForce, ForceMode2D.Impulse);
+        rb.AddForce(firePoint.up * slashForce, ForceMode2D.Impulse);
+        animator.SetTrigger("Attack");
     }
 }
