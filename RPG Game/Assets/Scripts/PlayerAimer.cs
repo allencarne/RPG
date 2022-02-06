8be02eb8554ab18b712416ad6d538ac883b54739
allencarne <allencarne@yahoo.com>
2022-02-05 18:31:49 -0800

    Added Slime Animations & updated player combat script
    I just moved a few lines from player aimer script into player combat.
    The animator.setfloat lines are not working still

--- RPG Game/Assets/Scripts/PlayerAimer.cs
+++ RPG Game/Assets/Scripts/PlayerAimer.cs
@@ -5,15 +5,7 @@ using UnityEngine;
 public class PlayerAimer : MonoBehaviour
 {
     public float offset;
-
     public Transform firePoint;
-    public GameObject slashPrefab;
-    public Animator animator;
-
-    public float slashForce;
-
-    private float attackCoolDown;
-    public float startAttackCoolDown;
 
     void Update()
     {
@@ -21,28 +13,5 @@ public class PlayerAimer : MonoBehaviour
         Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
-
-        if (attackCoolDown <= 0)
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                Attack();
-                attackCoolDown = startAttackCoolDown;
-                animator.SetFloat("Aim Horizontal", difference.x);
-                animator.SetFloat("Aim Vertical", difference.y);
-            }
-        }
-        else
-        {
-            attackCoolDown -= Time.deltaTime;
-        }
-    }
-
-    void Attack()
-    {
-        GameObject slash = Instantiate(slashPrefab, firePoint.position, firePoint.rotation); ;
-        Rigidbody2D rb = slash.GetComponent<Rigidbody2D>();
-        rb.AddForce(firePoint.up * slashForce, ForceMode2D.Impulse);
-        animator.SetTrigger("Attack");
     }
 }
--- RPG Game/Assets/Scripts/PlayerCombat.cs
+++ RPG Game/Assets/Scripts/PlayerCombat.cs
@@ -4,17 +4,39 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
-    PlayerAimer playerAimer;
+    public Transform firePoint;
+    public GameObject slashPrefab;
+    public Animator animator;
 
-    // Start is called before the first frame update
-    void Awake()
-    {
-        playerAimer = playerAimer.GetComponent<PlayerAimer>();
-    }
+    public float slashForce;
+
+    private float attackCoolDown;
+    public float startAttackCoolDown;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (attackCoolDown <= 0)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Attack();
+                attackCoolDown = startAttackCoolDown;
+            }
+        }
+        else
+        {
+            attackCoolDown -= Time.deltaTime;
+        }
+    }
+
+    void Attack()
+    {
+        animator.SetFloat("Aim Horizontal", firePoint.rotation.x);
+        animator.SetFloat("Aim Vertical", firePoint.rotation.y);
+        GameObject slash = Instantiate(slashPrefab, firePoint.position, firePoint.rotation); ;
+        Rigidbody2D rb = slash.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * slashForce, ForceMode2D.Impulse);
+        animator.SetTrigger("Attack");
     }
 }
