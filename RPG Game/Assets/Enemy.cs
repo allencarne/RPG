92f93c2f93ca78ee7624a8b79e3b40d5027173b3
allencarne <allencarne@yahoo.com>
2022-02-10 20:20:09 -0800

    Enemy Healthbar

--- RPG Game/Assets/Enemy.cs
+++ RPG Game/Assets/Enemy.cs
@@ -8,10 +8,13 @@ public class Enemy : MonoBehaviour
     public int maxHealth = 100;
     private int currentHealth;
 
+    public HealthBar healthBar;
+
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
@@ -23,6 +26,8 @@ public class Enemy : MonoBehaviour
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
 
         // Player hurt animation
         animator.SetTrigger("Hurt");
