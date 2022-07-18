813108cc5adb0964485637f6696d1201effa51a5
allencarne <allencarne@yahoo.com>
2022-07-17 19:40:06 -0700

    Dash

--- RPG Game/Assets/_Scripts/Player/PlayerStateMachine.cs
+++ RPG Game/Assets/_Scripts/Player/PlayerStateMachine.cs
@@ -43,6 +43,8 @@ public class PlayerStateMachine : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(state);
+
         switch (state)
         {
             case PlayerState.idle:
@@ -177,11 +179,55 @@ public class PlayerStateMachine : MonoBehaviour
     public void PlayerDashState()
     {
         //State Logic
+        animator.SetTrigger("Dash");
+
+        //Calculate the difference between mouse position and player position
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
+        if (!attackAnglePaused)
+        {
+            //Set Attack Animation Depending on Mouse Position
+            animator.SetFloat("Aim Horizontal", difference.x);
+            animator.SetFloat("Aim Vertical", difference.y);
+
+            //Set Idle to last attack position
+            animator.SetFloat("Horizontal", difference.x);
+            animator.SetFloat("Vertical", difference.y);
+
+            ////Slide Forward When Attacking
+            //Normalize movement vector and times it by attack move distance
+            difference = difference.normalized * attackMoveDistance;
+
+            //Add force in Attack Direction
+            rb.AddForce(difference, ForceMode2D.Impulse);
+
+            //Set AttackAnglePause Bool to True
+            attackAnglePaused = true;
+        }
+
+        if (isAttacking)
+        {
+            //Instantiate Slash prefab
+            GameObject slash = Instantiate(slashPrefab, firePoint.position, firePoint.rotation);
+
+            //Get the Rigid Body of the Slash prefab
+            Rigidbody2D slashRB = slash.GetComponent<Rigidbody2D>();
+
+            //Add Force to Slash prefab
+            slashRB.AddForce(firePoint.up * slashForce, ForceMode2D.Impulse);
+
+            //Reset Animator Trigger
+            animator.ResetTrigger("Dash");
+
+            //Reset isAttacking Bool;
+            isAttacking = false;
+        }
     }
 
     public void AttackAnimationEnd()
     {
         animator.ResetTrigger("Attack");
+        animator.ResetTrigger("Dash");
         attackAnglePaused = false;
         state = PlayerState.idle;
     }
