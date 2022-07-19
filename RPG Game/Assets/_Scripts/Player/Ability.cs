02d56cd685b906885d779cb1705eb28d6f1a10bd
allencarne <allencarne@yahoo.com>
2022-07-18 21:48:46 -0700

    Ability scriptable object

---
+++ RPG Game/Assets/_Scripts/Player/Ability.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ability : ScriptableObject
+{
+    public new string name;
+    public float coolDownTime;
+    public float activeTime;
+
+    public virtual void Activate(GameObject parent)
+    {
+
+    }
+}
