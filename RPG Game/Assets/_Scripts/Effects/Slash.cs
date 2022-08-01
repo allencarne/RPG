de66cf1863e7ebe08531e418fd91cf7e81273451
allencarne <allencarne@yahoo.com>
2022-07-31 19:02:25 -0700

    Mushroom art

--- RPG Game/Assets/_Scripts/Effects/Slash.cs
+++ RPG Game/Assets/_Scripts/Effects/Slash.cs
@@ -4,28 +4,23 @@ using UnityEngine;
 
 public class Slash : MonoBehaviour
 {
-    public WeaponScriptableObject weaponScriptableObject;
-
-    //public GameObject hitEffect;
-
-    //public int attackDamage = 1;
-    //[SerializeField] private float knockBackForce;
+    public PlayerScriptableObject playerScriptableObject;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Enemy")
         {
             // Hit Effect
-            GameObject effect = Instantiate(weaponScriptableObject.leftMouse1HitEffect, other.transform.position, Quaternion.identity);
+            GameObject effect = Instantiate(playerScriptableObject.weapon.leftMouse1HitEffect, other.transform.position, Quaternion.identity);
             Destroy(effect, .3f);
 
             // Deal Damage
-            other.GetComponent<Enemy>().TakeDamage(weaponScriptableObject.attackDamage);
+            other.GetComponent<Enemy>().TakeDamage(playerScriptableObject.weapon.attackDamage);
 
             // Apply Knockback
             Rigidbody2D enemy = other.GetComponent<Rigidbody2D>();
             Vector2 difference = enemy.transform.position - transform.position;
-            difference = difference.normalized * weaponScriptableObject.leftMouse1KnockBackForce;
+            difference = difference.normalized * playerScriptableObject.weapon.leftMouse1KnockBackForce;
             enemy.AddForce(difference, ForceMode2D.Impulse);
         }
     }
